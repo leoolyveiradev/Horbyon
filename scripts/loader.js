0f392cd5bdfaccdee209114d25a50d282d7ea0aa
Leonardo Oliveira <leonardoolliveiradev@gmail.com>
2026-02-17 00:23:21 -0300

    Delete scripts/loader.js

--- scripts/loader.js
+++
@@ -1,7 +0,0 @@
-// Esconde o loader depois que a página carregar
-window.addEventListener("load", () => {
-  const loader = document.getElementById("logo-carregando");
-  if (loader) {
-    loader.style.display = "none";
-  }
-});
