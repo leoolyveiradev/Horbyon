aa04503bab28ae65873c76264f09dd34ed65c367
Leonardo Oliveira <leonardoolliveiradev@gmail.com>
2026-02-17 00:25:33 -0300

    Refactor hamburger menu control logic

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,6 @@
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 const linkSobre = document.querySelector('a[href="sobre-a-horbyon.html"]');
-const logoCarregando = document.getElementById('logo-carregando');
 
 // Controle do menu hamburguer
 hamburger.addEventListener('click', () => {
@@ -19,19 +18,7 @@ document.querySelectorAll('.nav-links a').forEach(link => {
   }
 });
 
-// Animação da logo carregando ao clicar em "Sobre"
-linkSobre.addEventListener('click', event => {
-  event.preventDefault();
-
   // Fecha o menu hamburguer, caso esteja aberto
   navLinks.classList.remove('active');
   hamburger.classList.remove('open');
 
-  // Mostra a logo girando no centro da tela
-  logoCarregando.style.display = 'flex';
-
-  // Após 1.5 segundos redireciona para a página "Sobre"
-  setTimeout(() => {
-    window.location.href = linkSobre.href;
-  }, 1500);
-});
